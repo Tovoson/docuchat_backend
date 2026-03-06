c6035071574c6998b44e6364b5270c9d5ce7dcb3
Tovoson <elytovoniaina@gmail.com>
2026-03-06 15:33:15 +0300

    refactor: Externalize Jest configuration from `package.json` to `jest.config.js` and add `ts-jest` ESM transform.

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  testMatch: ["**/tests/**/*.test.ts"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
